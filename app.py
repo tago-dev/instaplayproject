daa1a8857bc4dd961d3b0593b3ef6b68f6d6f8e6
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-09-09 17:48:32 -0300

    Bug fixes and optimizations

--- app.py
+++ app.py
@@ -4,19 +4,22 @@ from pytube import YouTube
 
 app_title = '$ App: Universal YT Downloader'
 
+os('if not exist ".temp" mkdir ".temp" >nul 2>&1')
+os('if not exist "Downloaded Videos" mkdir "Downloaded Videos" >nul 2>&1')
+os('if not exist "Downloaded Musics" mkdir "Downloaded Musics" >nul 2>&1')
+os('if not exist "Downloaded Playlists" mkdir "Downloaded Playlists" >nul 2>&1')
+
 print()
 print(app_title)
 print()
 
 resolutions = {
-    1: '2160p',
-    2: '1440p',
-    3: '1080p',
-    4: '720p',
-    5: '480p',
-    6: '360p',
-    7: '240p',
-    8: '144p'
+    1: '1080p',
+    2: '720p',
+    3: '480p',
+    4: '360p',
+    5: '240p',
+    6: '144p'
 }
 
 print('Examples: ' + '   '.join([f'{k}. {v}' for k, v in resolutions.items()]))
@@ -45,16 +48,17 @@ video_title_mp4 = yt.title + '.mp4'
 for ch in '<>:"/\\|?*':
     video_title_mp4 = video_title_mp4.replace(ch, '')
 
-temp_video = '.temp/temp_video.mp4'
-temp_audio = '.temp/temp_audio.mp3'
-output = f'"Downloads/{video_title_mp4}"'
+temp_video = '.temp\\temp_video.mp4'
+temp_audio = '.temp\\temp_audio.mp3'
+output = f'"Downloaded Videos/{video_title_mp4}"'
 os(f'ffmpeg -i {temp_video} -i {temp_audio} -c copy {output} >nul 2>&1')
 os(f'ren "{output}" "{video_title_mp4}" >nul 2>&1')
 print('Merge Complete!')
 print()
 
 print('Deleting Temporary Files...')
-os('del /f /q ".temp\\temp_video.mp4" >nul 2>&1 && del /f /q ".temp\\temp_audio.mp3" >nul 2>&1')
+os('del /f /q ".temp\\temp_video.mp4" >nul 2>&1')
+os('del /f /q ".temp\\temp_audio.mp3" >nul 2>&1')
 print('Temporary Files Deleted!')
 print()
 
