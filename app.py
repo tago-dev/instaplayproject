af3b6b80b550f21e00c7c70b2053b381dddfc8cf
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-09-13 17:59:00 -0300

    Added Music Downloader

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 from os import makedirs
-from dependencies import download_video
-# import download_music
-# import download_playlist
-# import collect_data
-
-
-def coming_soon():
-    print()
-    print('Hahaha')
+from dependencies import download_video, download_music, download_playlist, video_channel_info
 
 
 app_title = '$ App: Universal YT Downloader'
@@ -15,12 +7,13 @@ app_title = '$ App: Universal YT Downloader'
 makedirs('Downloaded Videos', exist_ok=True)
 makedirs('Downloaded Musics', exist_ok=True)
 makedirs('Downloaded Playlists', exist_ok=True)
+makedirs('Video & Channel Info', exist_ok=True)
 
 options = {
     1: download_video.start,
-    2: coming_soon,
-    3: coming_soon,
-    4: coming_soon
+    2: download_music.start,
+    3: download_playlist,
+    4: video_channel_info,
 }
 
 print()
@@ -28,8 +21,8 @@ print(app_title)
 print()
 
 print('1. Video\n'
-      '2. Music (Coming Soon)\n'
+      '2. Music\n'
       '3. Playlist (Coming Soon)\n'
-      '4. Collect data (Coming Soon)\n')
+      '4. Video/Channel Info (Coming Soon)\n')
 option = int(input('Number: '))
 options[option]()
