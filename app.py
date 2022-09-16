1f1b7fa22d1aa7f05f62ebf8dbe61f969a997d42
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-09-15 21:13:42 -0300

    Project Update

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from os import makedirs
-from dependencies import download_video, download_music, download_playlist, video_channel_info
+from dependencies import download_video, download_music, download_playlist
 
 
 app_title = '$ App: Universal YT Downloader'
 
-makedirs('Downloaded Videos', exist_ok=True)
-makedirs('Downloaded Musics', exist_ok=True)
-makedirs('Downloaded Playlists', exist_ok=True)
-makedirs('Video & Channel Info', exist_ok=True)
+# makedirs('Downloaded Playlists', exist_ok=True)
 
 options = {
     1: download_video.start,
     2: download_music.start,
     3: download_playlist,
-    4: video_channel_info,
 }
 
 print()
@@ -22,7 +17,6 @@ print()
 
 print('1. Video\n'
       '2. Music\n'
-      '3. Playlist (Coming Soon)\n'
-      '4. Video/Channel Info (Coming Soon)\n')
+      '3. Playlist (Coming Soon)\n')
 option = int(input('Number: '))
 options[option]()
