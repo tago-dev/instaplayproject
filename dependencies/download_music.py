0774b732defe5e946a6b186083ba3def9fd9b788
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-10-03 16:07:23 -0300

    New function: add cover to music

--- dependencies/download_music.py
+++ dependencies/download_music.py
@@ -1,8 +1,10 @@
 from dependencies.functions import format_title
 from os import system as cmd, makedirs
-import os
-from pytube import YouTube
+from pytube import YouTube, extract
 from pytube.cli import on_progress
+import requests
+from shutil import rmtree
+import music_tag
 
 
 def start():
@@ -15,13 +17,28 @@ def start():
 
     # Downloading Audio...
     makedirs('Musics', exist_ok=True)
-    yt.streams.filter(only_audio=True).first().download(output_path='Musics', filename='temp_audio.mp3')
+    yt.streams.filter(only_audio=True).first().download(output_path='Musics', filename=format_title(yt.title) + '.mp3')
     print('[~] Audio Download Progress: |')
     print()
 
-    # Renaming the file...
-    video_title_mp3 = format_title(yt.title) + '.mp3'
-    os.rename('Musics\\temp_audio.mp3', f'Musics\\{video_title_mp3}')
+    # Adding cover...
+    makedirs('.temp', exist_ok=True)
+    yt_fomatted_title = format_title(YouTube(url).title)
+    yt_id = extract.video_id(url)
+    yt_thumbnail = f'https://img.youtube.com/vi/{yt_id}/maxresdefault.jpg'
+    r = requests.get(yt_thumbnail, allow_redirects=True)
+    open(f'.temp\\{yt_fomatted_title}.jpg', 'wb').write(r.content)
+
+    f = music_tag.load_file(f'Musics\\{yt_fomatted_title}.mp3')
+    f['artwork'] = open(f'.temp\\{yt_fomatted_title}.jpg', 'rb').read()
+    f.save()
+
+    # Deleting Temporary Files...
+    rmtree('.temp', ignore_errors=True)
+    print('[-] Temporary Files Deleted!')
+    print()
 
     print('[O] The music was downloaded successfully!')
     print()
+
+    # https://youtu.be/5z_SPHwspo8
