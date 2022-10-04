ef87c53fd008071109848018afe4665f14498372
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-10-04 11:23:14 -0300

    Added more characters to titles

--- dependencies/functions.py
+++ dependencies/functions.py
@@ -1,6 +1,6 @@
 def format_title(title):
     new_title = ''
     for ch in title:
-        if ch in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_()[]{} ':
+        if ch in 'áãâéêóõôúûÁÃÂÉÊÓÕÔÚÛabcdefghijklmnopqrstuvwxyzçABCDEFGHIJKLMNOPQRSTUVWXYZÇ0123456789-_()[]{} ':
             new_title += ch
     return new_title
