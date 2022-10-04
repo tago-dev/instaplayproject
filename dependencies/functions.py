a2461f90188e020bdc9b1c909ec0fc4dca159101
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-10-04 12:33:52 -0300

    Updated title formatting

--- dependencies/functions.py
+++ dependencies/functions.py
@@ -1,6 +1,11 @@
+from re import sub
+
+
 def format_title(title):
     new_title = ''
     for ch in title:
         if ch in 'áãâéêóõôúûÁÃÂÉÊÓÕÔÚÛabcdefghijklmnopqrstuvwxyzçABCDEFGHIJKLMNOPQRSTUVWXYZÇ0123456789-_()[]{} ':
             new_title += ch
+    new_title = sub(' +', ' ', new_title)
+    new_title = new_title.strip()
     return new_title
