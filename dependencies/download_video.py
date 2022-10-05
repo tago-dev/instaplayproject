655a9224e0d4f7f285d25a32aebb7784dd9e3de2
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-10-05 11:16:08 -0300

    Changed temp directory

--- dependencies/download_video.py
+++ dependencies/download_video.py
@@ -1,6 +1,6 @@
 from dependencies.functions import format_title
 from os import system as cmd, makedirs, environ, pathsep, path, getcwd
-from pytube import YouTube
+from pytube import YouTube, extract
 from pytube.cli import on_progress
 from ffmpeg import input as ffinput, output as ffoutput
 from shutil import rmtree
@@ -63,47 +63,50 @@ def start():
     yt = YouTube(url, on_progress_callback=on_progress)
 
     # Downloading FFMPEG...
-    makedirs('instaplayproject_dependencies', exist_ok=True)
-    ffmpeg_exe_zst = Path(r'instaplayproject_dependencies\ffmpeg.exe.zst')
+    userprofile_name = environ['userprofile']
+    makedirs(fr'{userprofile_name}\AppData\Local\Instaplay Project\dependencies', exist_ok=True)
+    ffmpeg_exe_zst = Path(fr'{userprofile_name}\AppData\Local\Instaplay Project\dependencies\ffmpeg.exe.zst')
     if not ffmpeg_exe_zst.is_file():
         cprint('[!] WARNING: The FFMPEG file will be downloaded ONLY the FIRST TIME you run this script! (Like now, please wait...)','red', attrs=['bold'])
         ffmpeg_url = 'https://drive.google.com/uc?export=download&id=16Ob9qv7uwLWqcMOwTOKeC9p52accn-wO'
         r = get(ffmpeg_url, allow_redirects=True)
-        open(r'instaplayproject_dependencies\ffmpeg.exe.zst', 'wb').write(r.content)
+        open(fr'{userprofile_name}\AppData\Local\Instaplay Project\dependencies\ffmpeg.exe.zst', 'wb').write(r.content)
         print()
 
     # Extracting FFMPEG...
-    makedirs('.temp', exist_ok=True)
-    with open(r'instaplayproject_dependencies\ffmpeg.exe.zst', mode='rb') as fi:
-        with open(r'.temp\ffmpeg.exe', mode='wb') as fo:
+    makedirs(fr'{userprofile_name}\AppData\Local\Instaplay Project\temp', exist_ok=True)
+    with open(fr'{userprofile_name}\AppData\Local\Instaplay Project\dependencies\ffmpeg.exe.zst', mode='rb') as fi:
+        with open(fr'{userprofile_name}\AppData\Local\Instaplay Project\temp\ffmpeg.exe', mode='wb') as fo:
             fo.write(ZSTD_uncompress(fi.read()))
-    environ['PATH'] += pathsep + path.join(getcwd(), '.temp')
+    environ['PATH'] += pathsep + path.join(getcwd(), fr'{userprofile_name}\AppData\Local\Instaplay Project\temp')
+
+    # Formatting the title...
+    video_title_mp4 = format_title(yt.title) + '.mp4'
+    video_title_mp3 = format_title(yt.title) + '.mp3'
 
     # Downloading Video...
-    yt.streams.filter(res=video_quality).first().download(output_path='.temp', filename='temp_video.mp4')
+    yt.streams.filter(res=video_quality).first().download(output_path=fr'{userprofile_name}\AppData\Local\Instaplay Project\temp', filename=video_title_mp4)
     print(f'{video_symbol_tilde} {video_text_beside1}|')
 
     # Downloading Audio...
-    yt.streams.filter(only_audio=True).first().download(output_path='.temp', filename='temp_audio.mp3')
+    yt.streams.filter(only_audio=True).first().download(output_path=fr'{userprofile_name}\AppData\Local\Instaplay Project\temp', filename=video_title_mp3)
     print(f'{video_symbol_tilde} {video_text_beside2}|')
     print()
 
     # Merging Video and Audio...
     makedirs('Videos', exist_ok=True)
-    video_title_mp4 = format_title(yt.title) + '.mp4'
-
-    temp_video = '.temp\\temp_video.mp4'
-    temp_audio = '.temp\\temp_audio.mp3'
-    output = f'Videos\\{video_title_mp4}'
+    temp_video = fr'{userprofile_name}\AppData\Local\Instaplay Project\temp\{video_title_mp4}'
+    temp_audio = fr'{userprofile_name}\AppData\Local\Instaplay Project\temp\{video_title_mp3}'
+    output = fr'Videos\{video_title_mp4}'
 
     input_video = ffinput(temp_video)
     input_audio = ffinput(temp_audio)
     ffoutput(input_video, input_audio, output, acodec='copy', vcodec='copy').run(quiet=True, overwrite_output=True)
-    print(f'{video_symbol_tilde} {video_text_beside3}|████████████████████████████████████████| 100.0 %')
+    print(f'{video_symbol_tilde} {video_text_beside3}|████████████████████████████████████████| 100.0%')
     print()
 
     # Deleting Temporary Files...
-    rmtree('.temp', ignore_errors=True)
+    rmtree(fr'{userprofile_name}\AppData\Local\Instaplay Project\temp', ignore_errors=True)
     print(f'{video_symbol_minus} {video_text_beside4}')
     print()
 
