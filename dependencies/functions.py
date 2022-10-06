23c74089bb4344032d4da8ecad12e35414d3ec2b
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-10-06 19:48:06 -0300

    Added more character possibilities in titles

--- dependencies/functions.py
+++ dependencies/functions.py
@@ -4,7 +4,7 @@ from re import sub
 def format_title(title):
     new_title = ''
     for ch in title:
-        if ch in 'áãâéêóõôúûÁÃÂÉÊÓÕÔÚÛabcdefghijklmnopqrstuvwxyzçABCDEFGHIJKLMNOPQRSTUVWXYZÇ0123456789-_()[]{} ':
+        if ch in 'aáàâãäåbcçdeéèêëfghiíìîïjklmnoóòôõöpqrstuúùûüvwxyzAÁÀÂÃÄÅBCÇDEÉÈÊËFGHIÍÌÎÏJKLMNOÓÒÔÕÖPQRSTUÚÙÛÜVWXYZ0123456789-_()[]{} ':
             new_title += ch
     new_title = sub(' +', ' ', new_title)
     new_title = new_title.strip()
